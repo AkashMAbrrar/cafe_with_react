9f025870f45e20ccc20c3a8092cb161e07f23e4f
AkashMAbrrar <madborakash48@gmail.com>
2024-10-23 23:13:28 +0600

    this commit for the projects initialized and added all the files set up the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h2 className="text-6xl text-center bg-red-400">
+        This Is Cafe Knowledge
+      </h2>
+    </>
+  );
+}
+
+export default App;
