95ec81420a5bebe5d63d39641ad86a0de992101c
AkashMAbrrar <madborakash48@gmail.com>
2024-10-24 17:53:13 +0600

    this commit for displayed all the items and information on the ui

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -13,6 +14,9 @@ const Blogs = () => {
   return (
     <div className="md:w-2/3">
       <h3>Here we create blogs : {blogs.length}</h3>
+      {blogs.map((blog) => (
+        <Blog blog={blog} key={blog.id}></Blog>
+      ))}
     </div>
   );
 };
